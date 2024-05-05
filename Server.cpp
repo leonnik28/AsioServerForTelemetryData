0f65cd49b98cdea9bdc8f71d9bd46d46b06b759b
leonnik28 <lencruseckij@gmail.com>
2024-05-06 00:19:12 +0300

    git commit -m "UpdateServerAndCreateFileProcessor"

--- Server.cpp
+++ Server.cpp
@@ -13,24 +13,28 @@
 #include <asio.hpp>
 #include <asio/buffer.hpp>
 #include <asio/read_until.hpp>
+#include <matplotlibcpp.h>
+#include <Eigen/Dense>
+#include <fstream>
+#include "TelemetryFileProcessor.h"
 
 class TelemetrySession : public std::enable_shared_from_this<TelemetrySession>
 {
 public:
-TelemetrySession(asio::ip::tcp::socket socket, std::shared_ptr<prometheus::Registry> registry)
-        : socket_(std::move(socket)),
-        registry_(registry)
+    TelemetrySession(asio::ip::tcp::socket socket, std::shared_ptr<prometheus::Registry> registry)
+            : socket_(std::move(socket)),
+              registry_(registry)
     {
         auto& sent_messages_counter_family = prometheus::BuildCounter()
-            .Name("telemetry_sent_messages_total")
-            .Help("Total number of sent telemetry messages")
-            .Register(*registry_);
+                .Name("telemetry_sent_messages_total")
+                .Help("Total number of sent telemetry messages")
+                .Register(*registry_);
         sent_messages_counter_ = &sent_messages_counter_family.Add({});
 
         auto& errors_counter_family = prometheus::BuildCounter()
-            .Name("telemetry_errors_total")
-            .Help("Total number of telemetry errors")
-            .Register(*registry_);
+                .Name("telemetry_errors_total")
+                .Help("Total number of telemetry errors")
+                .Register(*registry_);
         errors_counter_ = &errors_counter_family.Add({});
     }
 
@@ -39,56 +43,71 @@ TelemetrySession(asio::ip::tcp::socket socket, std::shared_ptr<prometheus::Regis
         do_read();
     }
 
+    std::string  VisualizeData() {
+        std::string filename = "telemetry_data.txt";
+        return TelemetryFileProcessor().VisualizeData(filename);
+    }
+
+    std::string StatisticalAnalysis() {
+        std::string filename = "telemetry_data.txt";
+        return TelemetryFileProcessor().StatisticalAnalysis(filename);
+    }
+
+    std::string Predict() {
+        std::string filename = "telemetry_data.txt";
+        return TelemetryFileProcessor().Predict(filename);
+    }
+
 private:
     void do_read()
     {
         auto self(shared_from_this());
         if (socket_.is_open()) {
             asio::async_read_until(socket_, buffer_, "",
-            [this, self](const std::error_code& ec, std::size_t length)
-            {
-                if (!ec)
-                {
-                    std::istream stream(&buffer_);
-                    std::string request;
-                    std::getline(stream, request); // Read the request from the stream
-                    std::cout << "Received: " << request << std::endl;
-                    buffer_.consume(length);
-                    handle_request(request);
-                    
-                }
-                else
-                {
-                    std::cout << "ERROR: " << ec << std::endl;
-                }
-            });
+                                  [this, self](const std::error_code& ec, std::size_t length)
+                                  {
+                                      if (!ec)
+                                      {
+                                          std::istream stream(&buffer_);
+                                          std::string request;
+                                          std::getline(stream, request); // Read the request from the stream
+                                          std::cout << "Received: " << request << std::endl;
+                                          buffer_.consume(length);
+                                          handle_request(request);
+                                      }
+                                      else
+                                      {
+                                          std::cout << "ERROR: " << ec << std::endl;
+                                      }
+                                  });
         }
     }
 
-void do_write(std::string message)
-{
-    auto self(shared_from_this());
-    message += "!";
-    std::cout << "Received: " << message << std::endl;
-    asio::async_write(socket_, asio::buffer(message),
-        [this, self](std::error_code ec, std::size_t /*length*/)
-        {
-            if (!ec)
-            {
-                sent_messages_counter_->Increment(); // Увеличение счетчика отправленных сообщений
-                do_read();
-            }
-            else
-            {
-                errors_counter_->Increment(); // Увеличение счетчика ошибок
-                std::cout << "ERROR" << std::endl;
-            }
-        });
-}
+    void do_write(std::string message)
+    {
+        auto self(shared_from_this());
+        message += "!";
+        std::cout << "Received: " << message << std::endl;
+        asio::async_write(socket_, asio::buffer(message),
+                         [this, self](std::error_code ec, std::size_t /*length*/)
+                         {
+                             if (!ec)
+                             {
+                                 sent_messages_counter_->Increment(); // Увеличение счетчика отправленных сообщений
+                                 do_read();
+                             }
+                             else
+                             {
+                                 errors_counter_->Increment(); // Увеличение счетчика ошибок
+                                 std::cout << "ERROR" << std::endl;
+                             }
+                         });
+    }
 
     std::string generate_telemetry_data()
     {
-        std::string telemetry_data;
+        std::string telemetryData;
+        std::string telemetryDataToFile;
 
         // Генерация нормально распределенных значений температуры и влажности
         std::random_device rd;
@@ -117,131 +136,62 @@ void do_write(std::string message)
         float temperatureFactor = 1.0 + 0.2 * std::sin(2 * M_PI * hour / 24.0);
         temperature = static_cast<int>(temperature * temperatureFactor);
 
-        telemetry_data += "Temperature: " + std::to_string(temperature) + " C\n";
-        telemetry_data += "Humidity: " + std::to_string(humidity) + " %\n";
+        telemetryData += "Temperature: " + std::to_string(temperature) + " C\n";
+        telemetryData += "Humidity: " + std::to_string(humidity) + " %\n";
 
-        return telemetry_data;
+        telemetryDataToFile += std::to_string(temperature) + " " + std::to_string(humidity) + "\n";
+
+        std::string filename = "telemetry_data.txt";
+        std::ofstream outfile(filename, std::ios_base::app);
+        outfile << telemetryDataToFile;
+        outfile.close();
+        
+        return telemetryData;
     }
 
-int generate_random_number(int min, int max)
-{
-    static bool first = true;
-    if (first) {
-        srand(time(NULL));
-        first = false;
-    }
-    return min + rand() % ((max + 1) - min);
-}
+    void handle_request(std::string request)
+    {
+        std::string response;
 
-void handle_request(std::string request)
-{
-    std::string response;
-
-    if (request == "ECHO")
-    {
-        response = request + "\n";
-    }
-    else if (request == "QUIT")
-    {
-        socket_.close();
-        return;
-    }
-    else if (request == "INFO")
-    {
-        response = "Server information ... : \n";
-    }
-    else if (request == "LIST")
-    {
-        std::string current_directory = getCurrentDirectory();
-        response = listDirectory(current_directory);
-    }
-    else if (request.substr(0, 3) == "CD ")
-    {
-        std::string directory = request.substr(3);
-        bool success = changeDirectory(directory);
-        if (success)
+        if (request == "QUIT")
         {
-            std::string current_directory = getCurrentDirectory();
-            response = (std::string)"Directory changed to: " + current_directory + "\n";
+            socket_.close();
+            return;
+        }
+        else if (request == "INFO")
+        {
+            response = "Server information ... : \n";
+        }
+        else if (request == "TELEMETRYDATA")
+        {
+            response = generate_telemetry_data();
+        }
+        else if (request == "METRICKS")
+        {
+            response = std::to_string(sent_messages_counter_->Value()) + "\n";
+        }
+        else if (request == "VISUALIZE") {
+            response = VisualizeData();
+        }
+        else if (request == "STATISTICS") {
+            response = StatisticalAnalysis();
+        }
+        else if (request == "PREDICT") {
+            response = Predict();
         }
         else
         {
-            response = (std::string)"Failed to change directory\n";
+            response = updateToString("Unknown request\n");
         }
-    }
-    else if (request == "TELEMETRYDATA")
-    {
-        response = generate_telemetry_data();
-    }
-    else if (request == "METRICKS")
-    {
-        response = std::to_string(sent_messages_counter_->Value()) + "\n";
-    }
-    else
-    {
-        response = updateToString("Unknown request\n");
+
+        do_write(response);
     }
 
-    do_write(response);
-}
-
-std::string updateToString(const char* string){
-    std::string newString;
-    newString = string;
-    return newString;
-}
-
-std::string getCurrentDirectory()
-{
-    char* buffer = get_current_dir_name();
-    std::string current_directory(buffer);
-    free(buffer);
-    return current_directory;
-}
-
-std::string listDirectory(const std::string& directory)
-{
-    std::vector<std::string> contents = getDirectoryContents(directory);
-    std::string result;
-
-    for (const std::string& entry : contents)
-    {
-        result += entry + "\n";
+    std::string updateToString(const char* string){
+        std::string newString;
+        newString = string;
+        return newString;
     }
-
-    return result;
-}
-
-std::vector<std::string> getDirectoryContents(const std::string& directory)
-{
-    std::vector<std::string> contents;
-    DIR* dir = opendir(directory.c_str());
-    if (dir)
-    {
-        struct dirent* entry;
-        while ((entry = readdir(dir)) != nullptr)
-        {
-            std::string name(entry->d_name);
-            // Игнорируем текущий и родительский каталоги
-            if (name != "." && name != "..")
-            {
-                contents.push_back(name);
-            }
-        }
-        closedir(dir);
-    }
-    return contents;
-}
-
-bool changeDirectory(const std::string& directory)
-{
-    if (chdir(directory.c_str()) == 0)
-    {
-        return true;
-    }
-    return false;
-}
-
 
     asio::ip::tcp::socket socket_;
     asio::streambuf buffer_;
@@ -265,6 +215,11 @@ public:
         return registry_;
     }
 
+    void stop()
+    {
+        acceptor_.close();
+    }
+
 private:
     void do_accept()
     {
@@ -284,6 +239,20 @@ private:
     std::shared_ptr<prometheus::Registry> registry_;
 };
 
+static TelemetryServer* server_ptr = nullptr;
+static asio::io_service* io_service_ptr;
+
+void signal_handler(int signal)
+{
+    if (server_ptr != nullptr)
+    {
+        std::cout << "\nSignal " << signal << " received, shutting down server...\n";
+        server_ptr->stop();
+        io_service_ptr->stop();
+    }
+}
+
+
 int main(int argc, char* argv[])
 {
     try
@@ -295,13 +264,18 @@ int main(int argc, char* argv[])
         }
 
         asio::io_service io_service;
+        io_service_ptr = &io_service;
 
-        TelemetryServer s(io_service, std::atoi(argv[1]));
+        TelemetryServer server(io_service, std::atoi(argv[1]));
+        server_ptr = &server;
 
         prometheus::Exposer exposer("localhost:8080");
-        exposer.RegisterCollectable(s.getRegistry());
+        exposer.RegisterCollectable(server.getRegistry());
+
+        std::signal(SIGINT, signal_handler);
 
         io_service.run();
+
     }
     catch (std::exception& e)
     {
---
+++ TelemetryFileProcessor.h
@@ -0,0 +1,105 @@
+#include <string>
+#include <vector>
+#include <matplotlibcpp.h>
+#include <Eigen/Dense>
+#include <fstream>
+
+class TelemetryFileProcessor {
+public:
+    std::string VisualizeData(const std::string& filename) {
+        std::ifstream infile(filename);
+        std::vector<double> x, y;
+        double temp_x, temp_y;
+        while (infile >> temp_x >> temp_y) {
+            x.push_back(temp_x);
+            y.push_back(temp_y);
+        }
+        matplotlibcpp::plot(x, y);
+        matplotlibcpp::show();
+
+        std::stringstream result;
+
+        result << "Visualize Comliete\n";
+        return result.str();
+
+    }
+
+    std::string StatisticalAnalysis(const std::string& filename) {
+        std::ifstream infile(filename);
+        std::vector<double> data1, data2;
+        double temp1, temp2;
+        while (infile >> temp1 >> temp2) {
+            data1.push_back(temp1);
+            data2.push_back(temp2);
+        }
+        Eigen::VectorXd v1(data1.size());
+        Eigen::VectorXd v2(data2.size());
+        for (int i = 0; i < data1.size(); i++) {
+            v1(i) = data1[i];
+        }
+        for (int i = 0; i < data2.size(); i++) {
+            v2(i) = data2[i];
+        }
+
+        double mean1 = v1.mean();
+        double squared_diffs_sum1 = (v1.array() - mean1).pow(2).sum();
+        int n1 = v1.size();
+        double stddev1 = std::sqrt(squared_diffs_sum1 / (n1 - 1));
+
+        double mean2 = v2.mean();
+        double squared_diffs_sum2 = (v2.array() - mean2).pow(2).sum();
+        int n2 = v2.size();
+        double stddev2 = std::sqrt(squared_diffs_sum2 / (n2 - 1));
+
+        std::stringstream result;
+        result << "Temperature:\n";
+        result << "Mean: " << mean1 << "\n";
+        result << "Standard deviation: " << stddev1 << "\n";
+        result << "Humidity:\n";
+        result << "Mean: " << mean2 << "\n";
+        result << "Standard deviation: " << stddev2 << "\n";
+        return result.str();
+    }
+
+    std::string Predict(const std::string& filename) {
+        std::ifstream infile(filename);
+        std::vector<double> data1, data2;
+        double temp1, temp2;
+        while (infile >> temp1 >> temp2) {
+            data1.push_back(temp1);
+            data2.push_back(temp2);
+        }
+        Eigen::VectorXd v1(data1.size());
+        Eigen::VectorXd v2(data2.size());
+        for (int i = 0; i < data1.size(); i++) {
+            v1(i) = data1[i];
+        }
+        for (int i = 0; i < data2.size(); i++) {
+            v2(i) = data2[i];
+        }
+        Eigen::VectorXd coeff1 = Eigen::VectorXd::Ones(2);
+        Eigen::MatrixXd A1(data1.size(), 2);
+        for (int i = 0; i < data1.size(); i++) {
+            A1(i, 0) = i;
+            A1(i, 1) = 1;
+        }
+        coeff1 = (A1.transpose() * A1).inverse() * A1.transpose() * v1;
+        double predicted_value1 = coeff1(0) * data1.size() + coeff1(1);
+
+        Eigen::VectorXd coeff2 = Eigen::VectorXd::Ones(2);
+        Eigen::MatrixXd A2(data2.size(), 2);
+        for (int i = 0; i < data2.size(); i++) {
+            A2(i, 0) = i;
+            A2(i, 1) = 1;
+        }
+        coeff2 = (A2.transpose() * A2).inverse() * A2.transpose() * v2;
+        double predicted_value2 = coeff2(0) * data2.size() + coeff2(1);
+
+        std::stringstream result;
+        result << "Temperature:\n";
+        result << "Predicted value: " << predicted_value1 << "\n";
+        result << "Humidity:\n";
+        result << "Predicted value: " << predicted_value2 << "\n";
+        return result.str();
+    }
+};
